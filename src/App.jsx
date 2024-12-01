7c2e1d36dca0db2b20e3242ba04b4026f7238a70
Max Gonzalez Roy <groymax1@gmail.com>
2024-12-01 19:08:19 -0300

    Se crean componentes archivo CartWidget.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div>
             <NavBar />
-            <ItemListContainer greeting="¡Bienvenido a nuestra tienda online!" />
+            <ItemListContainer greeting="¡Bienvenido a nuestra tienda online!"/>
         </div>
     );
 }
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CartWidget = () => {
+    return (
+        <div style={{ display: 'flex', alignItems: 'center', gap: '5px'}}>
+            <img src="https://img.icons8.com/ios-filled/50/shopping-cart.png" alt="Carrito de compras" style={{ width: '24px'}}/>
+            <span style={{ backgroundColor: 'red', color: 'white', borderRadius: '50%', padding: '5px 10px'}}>33</span>
+        </div>
+    );
+};
+
+export default CartWidget;
+
